f51aa9fd2cb613a229eefd786f747b4f0fcefa93
Andrea Bellver <84071511+ponisworld@users.noreply.github.com>
2022-12-02 22:26:35 +0100

    First try of Basic Carousel !!!
    Exercise from Front End Masters, first try.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
-const mainImages = document.querySelectorAll("main > img");
+const mainImages = document.querySelectorAll("section > img");
 const buttons = document.querySelectorAll("button");
 
 let currentImage = 0;
-let imageToDisplay = mainImages[currentImage];
 
 const prevButton = document.querySelector("#prevArrow");
 const nextButton = document.querySelector("#nextArrow");
 
+mainImages[currentImage].classList.add("active");
+
 prevButton.addEventListener( "click",
     () => {
-        imageToDisplay.classList.remove("active");
+        console.log(currentImage)
+        mainImages[currentImage].classList.remove("active");
+        currentImage--;
+        mainImages[currentImage].classList.add("active");
+        nextButton.disabled = false;
+
+        if (currentImage === 0) {
+            prevButton.disabled = true;
+        }
     }
 )
 
-init ();
+nextButton.addEventListener( "click" ,
+    () => {
+        
+        mainImages[currentImage].classList.remove("active");
+        currentImage++;
+        mainImages[currentImage].classList.add("active");
+        prevButton.disabled = false;
+
+        if (currentImage === mainImages.length - 1) {
+            nextButton.disabled = true;
+        }
+    }
+)
